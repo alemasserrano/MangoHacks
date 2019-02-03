e08ff0d6cf9ee39d090ccd9732631a31671f3908
deellah <abdullah.diane@gmail.com>
2019-02-02 22:52:49 -0500

    added camera functionality

--- app/src/main/java/com/example/mangohacks/MainActivity.java
+++ app/src/main/java/com/example/mangohacks/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.mangohacks;
 
+import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    Button button;
+    static final int CAM_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(camera_intent, CAM_REQUEST);
+            }
+        });
+    }}
